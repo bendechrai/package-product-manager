70ee16b7af9cc736e67fbc4956ace43c589e5d00
Ben Dechrai <ben@dechrai.com>
2016-05-20 13:55:48 +1000

    Use correct module name

--- code/Block/Adminhtml/Import/Grid.php
+++ code/Block/Adminhtml/Import/Grid.php
@@ -22,20 +22,20 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Import_Grid extends Mage_
   protected function _prepareColumns()
   {
       $this->addColumn('sku', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('SKU'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('SKU'),
           'align'     => 'left',
           'index'     => 'sku',
       ));
 
       $this->addColumn('associated_products', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('Associated Products'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('Associated Products'),
           'align'     => 'left',
           'index'     => 'associated_products',
           'renderer'  => 'BenDechrai_PackageProductManager_Block_Adminhtml_Template_Grid_Renderer_AssociatedProducts',
       ));
 
       $this->addColumn('catalog_product_exists', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('Catalog Product Exists?'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('Catalog Product Exists?'),
           'align'     => 'center',
           'index'     => 'catalog_product_exists',
           'type'      => 'options',
@@ -46,7 +46,7 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Import_Grid extends Mage_
       ));
 
       $this->addColumn('replace_existing', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('Replace Existing?'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('Replace Existing?'),
           'align'     => 'center',
           'index'     => 'replace_existing',
           'type'      => 'options',
@@ -57,7 +57,7 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Import_Grid extends Mage_
       ));
 
       $this->addColumn('approved', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('Approved'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('Approved'),
           'align'     => 'center',
           'index'     => 'approved',
           'type'      => 'options',
@@ -68,7 +68,7 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Import_Grid extends Mage_
       ));
 
       $this->addColumn('listed', array(
-          'header'    => Mage::helper('runautoparts_partsdb')->__('Listed'),
+          'header'    => Mage::helper('bendechrai_packageproductmanager')->__('Listed'),
           'align'     => 'center',
           'index'     => 'listed',
           'type'      => 'options',
@@ -89,21 +89,21 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Import_Grid extends Mage_
     $this->getMassactionBlock()->setFormFieldName('package_id');
 
     $this->getMassactionBlock()->addItem('Approve', array(
-      'label'    => Mage::helper('runautoparts_partsdb')->__('Approve'),
+      'label'    => Mage::helper('bendechrai_packageproductmanager')->__('Approve'),
       'url'      => $this->getUrl('*/*/massApprovePackages'),
-      'confirm'  => Mage::helper('runautoparts_partsdb')->__('This will mark these packages as approved, and could result in them being added to the store catalog. Are you sure?')
+      'confirm'  => Mage::helper('bendechrai_packageproductmanager')->__('This will mark these packages as approved, and could result in them being added to the store catalog. Are you sure?')
     ));
 
     $this->getMassactionBlock()->addItem('Unapprove', array(
-      'label'    => Mage::helper('runautoparts_partsdb')->__('Unapprove'),
+      'label'    => Mage::helper('bendechrai_packageproductmanager')->__('Unapprove'),
       'url'      => $this->getUrl('*/*/massUnapprovePackages'),
-      'confirm'  => Mage::helper('runautoparts_partsdb')->__('This will unapprove these packages. It will not remove any packages already added to the catalog. Are you sure?')
+      'confirm'  => Mage::helper('bendechrai_packageproductmanager')->__('This will unapprove these packages. It will not remove any packages already added to the catalog. Are you sure?')
     ));
 
     $this->getMassactionBlock()->addItem('Refresh', array(
-      'label'    => Mage::helper('runautoparts_partsdb')->__('Refresh'),
+      'label'    => Mage::helper('bendechrai_packageproductmanager')->__('Refresh'),
       'url'      => $this->getUrl('*/*/massRefreshPackages'),
-      'confirm'  => Mage::helper('runautoparts_partsdb')->__('This will refresh the packages with data from the current catalog. The catalog will not be updated. Are you sure?')
+      'confirm'  => Mage::helper('bendechrai_packageproductmanager')->__('This will refresh the packages with data from the current catalog. The catalog will not be updated. Are you sure?')
     ));
 
     return $this;
