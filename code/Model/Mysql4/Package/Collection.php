2dc6d13cbdcf8e01a8f318aeb9787f4de17a90c5
Ben Dechrai <ben@dechrai.com>
2016-05-20 17:21:31 +1000

    Helper medhot to get packages that are ready to be listed

--- code/Model/Mysql4/Package/Collection.php
+++ code/Model/Mysql4/Package/Collection.php
@@ -6,4 +6,14 @@ class BenDechrai_PackageProductManager_Model_Mysql4_Package_Collection extends M
     $this->_init('bendechrai_packageproductmanager/package');
   }
 
+  public function limitToList() {
+    $this->addFieldToFilter('approved', '1');
+    $this->addFieldToFilter('listed', '0');
+
+    // Ensure, if catalog product exists, it can be replaced
+    $this->getSelect()->where('catalog_product_exists=0 OR catalog_product_exists=replace_existing');
+
+    return $this;
+  }
+
 }
