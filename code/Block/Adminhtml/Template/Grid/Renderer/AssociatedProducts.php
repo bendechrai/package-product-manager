6cad1153cdb636f5d4ef64c14834e78d110a22d0
Ben Dechrai <ben@dechrai.com>
2016-05-30 12:22:15 +1000

    Add price detail to Associated Products column

--- code/Block/Adminhtml/Template/Grid/Renderer/AssociatedProducts.php
+++ code/Block/Adminhtml/Template/Grid/Renderer/AssociatedProducts.php
@@ -13,7 +13,14 @@ class BenDechrai_PackageProductManager_Block_Adminhtml_Template_Grid_Renderer_As
 
     foreach($products as $product) {
       $out .= "
-        <div class=\"bd-ppm-grid-product\">{$product->getQty()} x {$product->getCatalogProduct()->getName()}</div>
+        <div class=\"bd-ppm-grid-product\">
+          {$product->getQty()}
+          x
+          <a href=\"{$this->getUrl('*/catalog_product/edit', array('id'=>$product->getCatalogProduct()->getId()))}\"
+            title=\"{$product->getCatalogProduct()->getName()}\">{$product->getCatalogProduct()->getName()}</a>
+          @
+          $" . sprintf("%0.2f", $product->getCatalogProduct()->getPrice()) . "
+        </div>
       ";
     }
 
